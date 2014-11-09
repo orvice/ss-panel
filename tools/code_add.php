a244ad06fed26d072442b621fa96d432a3a721b3
orvice <orvice@gmail.com>
2014-11-10 01:31:33 +0800

    add tools

---
+++ tools/code_add.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '../lib/config.php';
+require_once '../lib/comm.func.php';
+require_once '../lib/user.func.php';
+require_once '../lib/reg.func.php';
+require_once '../lib/invite_code.class.php';
+
+$num = 10;
+//$user = $_GET['user'];
+$user = 0;
+for($a=0;$a<=$num;$a++){
+    echo $a."----";
+    $x = rand(10,1000);
+    $x = md5($x);
+    $code = "00".substr($x,rand(1,13),14);
+    echo $code."===by user".$user."</br>";
+    $sql = "INSERT INTO `invite_code` (`id`, `code`, `user`) VALUES (NULL, '$code', '$user');";
+    $dbc->query($sql);
+}

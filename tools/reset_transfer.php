3cc417cd67f0aa69f1daf414f4c09b9cfd98b484
orvice <orvice@gmail.com>
2014-11-10 02:15:29 +0800

    remove unused require

--- tools/reset_transfer.php
+++ tools/reset_transfer.php
@@ -3,10 +3,5 @@
  * 清空流量
  */
 require_once '../lib/config.php';
-require_once '../lib/comm.func.php';
-require_once '../lib/user.func.php';
-require_once '../lib/reg.func.php';
-require_once '../lib/invite_code.class.php';
-
 $sql = "UPDATE `user` SET `u` = '0', `d` = '0' ";
 $dbc->query($sql);

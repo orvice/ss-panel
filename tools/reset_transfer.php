3979b8603743e74a6a8ec95ffc9ef61dc9c05c6e
orvice <orvice@gmail.com>
2014-11-10 17:45:36 +0800

    init cron

--- tools/reset_transfer.php
+++ tools/reset_transfer.php
@@ -2,6 +2,13 @@
 /*
  * 清空流量
  */
-require_once '../lib/config.php';
+
+
+//定义清零日期,1为每月1号
+$reset_date = '1';
+
+//日期符合就清零
 $sql = "UPDATE `user` SET `u` = '0', `d` = '0' ";
-$dbc->query($sql);
+if (date('d')==$reset_date){
+    $dbc->query($sql);
+}

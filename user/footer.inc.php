452ce61a311d3e961fc6741ae9731cd6805e1b73
orvice <orvice@gmail.com>
2014-11-17 16:13:56 +0800

    fix js path

--- user/footer.inc.php
+++ user/footer.inc.php
@@ -11,16 +11,16 @@
 
 
 <!-- Load JS here for greater good
-<script src="/<?php echo BD_PATH; ?>js/jquery-1.8.3.min.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/jquery-ui-1.10.3.custom.min.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/jquery.ui.touch-punch.min.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/bootstrap.min.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/bootstrap-select.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/bootstrap-switch.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/flatui-checkbox.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/flatui-radio.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/jquery.tagsinput.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/jquery.placeholder.js"></script>
-<script src="/<?php echo BD_PATH; ?>js/custom_checkbox_and_radio.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/jquery-1.8.3.min.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/jquery-ui-1.10.3.custom.min.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/jquery.ui.touch-punch.min.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/bootstrap.min.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/bootstrap-select.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/bootstrap-switch.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/flatui-checkbox.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/flatui-radio.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/jquery.tagsinput.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/jquery.placeholder.js"></script>
+<script src="/<?php echo SS_PATH; ?>js/custom_checkbox_and_radio.js"></script>
 =============================-->
 

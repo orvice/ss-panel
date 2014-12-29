5079e4f3548900e0d67210fc890cf11f29d31599
orvice <orvice@gmail.com>
2014-12-29 23:23:23 +0800

    从数据库中读取node

--- user/node.php
+++ user/node.php
@@ -5,7 +5,7 @@ require_once 'user_check.php';
 <!DOCTYPE html>
 <html>
 <head>
-    <title>ShadowX</title>
+    <title><?php echo $site_name;?></title>
     <?php include_once 'lib/header.inc.php'; ?>
 </head>
 <body class="skin-blue">
@@ -17,18 +17,13 @@ include_once 'lib/slidebar_left.inc.php';
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            用户中心
-            <small>User Panel</small>
+            节点列表
+            <small>Node List</small>
         </h1>
-        <ol class="breadcrumb">
-            <li><a href="index.php"><i class="fa fa-dashboard"></i>ShadowX</a></li>
-            <li class="active">UserCenter</li>
-        </ol>
     </section>
     <!-- Main content -->
     <section class="content">
         <!-- START PROGRESS BARS -->
-        <h2 class="page-header">Node List</h2>
         <div class="row">
             <div class="col-md-6">
                 <div class="box box-solid">
@@ -40,15 +35,53 @@ include_once 'lib/slidebar_left.inc.php';
                         <div class="callout callout-info">
                             <h4>加密方式</h4>
                             <p>无特殊说明加密方式均为<code>aes-256-cfb</code></p>
-                        </div>
-                        <h4>美国</h4>
-                        <p>1号:  <code>ip</code> </p>
-                         
+                        </div><?php
+                        $sql ="SELECT * FROM `ss_node` WHERE `node_type` = '0' ORDER BY node_order ";
+                        $query =  $dbc->query($sql);
+                        while ( $rs = $query->fetch_array()){
+                        ?>
+                        <p><?php echo $rs['node_name']; ?>:  <code><?php echo $rs['node_server']; ?></code><?php echo $rs['node_info']; ?> </p>
+                        <?php }?>
                     </div><!-- /.box-body -->
                 </div><!-- /.box -->
             </div><!-- /.col (left) -->
 
-           
+            <div class="col-md-6">
+                <div class="box box-solid">
+                    <div class="box-header">
+                        <i class="fa fa-code"></i>
+                        <h3 class="box-title">测试节点</h3>
+                    </div><!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="callout callout-warning">
+                            <h4>注意!</h4>
+                            <p>测试节点可能随时撤销，有问题请反馈.</p>
+                        </div><?php
+                        $sql ="SELECT * FROM `ss_node` WHERE `node_type` = '1' ORDER BY node_order ";
+                        $query =  $dbc->query($sql);
+                        while ( $rs = $query->fetch_array()){
+                            ?>
+                            <p><?php echo $rs['node_name']; ?>:  <code><?php echo $rs['node_server']; ?></code><?php echo $rs['node_info']; ?> </p>
+                        <?php }?>
+                    </div><!-- /.box-body -->
+                </div><!-- /.box -->
+            </div><!-- /.col (right) -->
+            <div class="col-md-6">
+                <div class="box box-solid">
+                    <div class="box-header">
+                        <i class="fa fa-align-left"></i>
+                        <h3 class="box-title">不限流量节点</h3>
+                    </div><!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="callout callout-info">
+                            <h4>说明</h4>
+                            <p>使用此端口和密码不限制流量，将不会占用你ShadowX账户的流量。</p>
+                            <p>暂时关闭</p>
+                        </div>
+                    </div><!-- /.box-body -->
+                </div><!-- /.box -->
+            </div><!-- /.col (right) -->
+        </div><!-- /.row -->
         <!-- END PROGRESS BARS -->
     </section><!-- /.content -->
 </aside><!-- /.right-side -->

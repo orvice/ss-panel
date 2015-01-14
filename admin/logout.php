1ef13bd78ac5fc2615ac1f86d6c6f5f412033be7
orvice <orvice@gmail.com>
2015-01-14 16:54:13 +0800

    fix #23

--- admin/logout.php
+++ admin/logout.php
@@ -3,9 +3,9 @@ session_start();
 
 //注销登录
 if($_GET['action'] == "logout"){
-    setcookie("user_name", "", time()-3600);
-    setcookie("user_pwd", "", time()-3600);
-    setcookie("user_uid", "", time()-3600);
+    setcookie("admin_name", "", time()-3600);
+    setcookie("admin_pwd", "", time()-3600);
+    setcookie("admin_uid", "", time()-3600);
     header("Location:index.php");
     exit;
 }

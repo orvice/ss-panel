292f6dbdb316e6ed0803c7fdc9885469cb1d037e
icodex <jack@evlit.com>
2015-02-10 20:55:59 +0800

    Create user.class.php

---
+++ lib/class/user.class.php
@@ -0,0 +1,32 @@
+<?php
+
+ class user {
+
+     public $uid;
+     private $dbc;
+
+     function __construct($uid){
+         global $dbc;
+         $this->uid = $uid;
+         $this->dbc = $dbc;
+     }
+
+     function update($user_name,$user_email,$user_pass,$user_passwd,$transfer_enable){
+         $sql = " UPDATE `user` SET
+                  `user_name` = '$user_name',
+                  `email` = '$user_email',
+                  `pass` = '$user_pass',
+                  `passwd` = '$user_passwd',
+                  `transfer_enable` = '$transfer_enable'
+                  WHERE  `uid` = '$this->uid' ";
+         $query = $this->dbc->query($sql);
+         return $query;
+     }
+
+     function del(){
+         $sql = "DELETE FROM `user` WHERE `uid` = $this->uid ";
+         $query = $this->dbc->query($sql);
+         return $query;
+     }
+
+}

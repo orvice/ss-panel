1d908f60b1011d5723b20bad3193204580dd8646
orvice <orvice@gmail.com>
2015-02-25 20:21:35 +0800

    ad AdminLTE js

--- user/lib/footer.inc.php
+++ user/lib/footer.inc.php
@@ -1,2 +1,4 @@
 <script src="../js/jquery-2.0.3.min.js"></script>
 <script src="../js/bootstrap.min.js" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="../js/AdminLTE/app.js" type="text/javascript"></script>

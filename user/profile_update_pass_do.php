fb94d8ae339f83a1f695dbe9f1f376215fea18c1
icodex <jack@evlit.com>
2015-03-02 20:36:01 +0800

    Update profile_update_pass_do.php

--- user/profile_update_pass_do.php
+++ user/profile_update_pass_do.php
@@ -13,6 +13,10 @@ if(empty($_POST['pass'])){
     header("Location:index.php");
 }else{ 
     $pass = mysqli_real_escape_string($dbc,trim($_POST['pass']));
+    $transfer = $oo->get_transfer_enable();
+    $oo->set_transfer('-99999');
+    sleep(10);
     $oo->update_ss_pass($pass);
+    $oo->set_transfer($transfer);
     echo " <script>window.location='index.php';</script> " ;
 }

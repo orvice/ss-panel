312dd1f2515565bec88bfcf4520af86679dfac8d
orvice <orvice@gmail.com>
2015-04-26 02:08:04 +0800

    update Nodes

--- lib/Ss/Node/Node.php
+++ lib/Ss/Node/Node.php
@@ -13,7 +13,15 @@ namespace Ss\Node;
          $this->db  = $db;
      }
 
-     function add($node_name,$node_type,$node_server,$node_method,$node_info,$node_status,$node_order){
+     function AllNode(){
+         $node_array = $this->db->select("ss_node","*",[
+             "ORDER" => "node_order"
+             //"LIMIT" => 21
+         ]);
+         return $node_array;
+     }
+
+     function Add($node_name,$node_type,$node_server,$node_method,$node_info,$node_status,$node_order){
          $this->db->insert("ss_node", [
              "node_name" => $node_name,
              "node_type" => $node_type,
@@ -26,29 +34,17 @@ namespace Ss\Node;
          return 1;
      }
 
-     function update($node_name,$node_type,$node_server,$node_method,$node_info,$node_status,$node_order){
-         $this->db->update("ss_node", [
-             "node_name" => $node_name,
-             "node_type" => $node_type,
-             "node_server" => $node_server,
-             "node_method" => $node_method,
-             "node_info" => $node_info,
-             "node_status" => $node_status,
-             "node_order" =>  $node_order
-         ],[
-           "id[=]"  => $this->id
-         ]);
-         return 1;
-     }
-
-     function del(){
-         $this->db->delete("ss_node",[
-             "id[=]"  => $this->id
-         ]);
-         return 1;
-     }
 
      function get_node_array($node_type){
+         $node_array = $this->db->select("ss_node","*",[
+             "node_type[=]" => $node_type,
+             "ORDER" => "node_order",
+             //"LIMIT" => 21
+         ]);
+         return $node_array;
+     }
+
+     function NodesArray($node_type){
          $node_array = $this->db->select("ss_node","*",[
              "node_type[=]" => $node_type,
              "ORDER" => "node_order",

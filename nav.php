6e44e4b6d01c7a60e0ec9ff52e543234dd8c0bba
orvice <orvice@gmail.com>
2015-04-26 11:56:17 +0800

    update nav

--- nav.php
+++ nav.php
@@ -1,7 +1,6 @@
 <div class="header">
     <ul class="nav nav-pills pull-right" role="tablist">
-        <li role="presentation"><a href="index.php">Home</a></li>
-        <li role="presentation"><a href="https://plus.google.com/u/0/communities/105993338677212487777">Forum</a></li>
+        <li role="presentation"><a href="index.php">Home</a></li> 
         <li role="presentation"><a href="faq.php">FAQ</a></li>
         <li role="presentation"><a href="user/register.php">Reg</a></li>
         <li role="presentation"><a href="user/index.php">UserCenter</a></li>

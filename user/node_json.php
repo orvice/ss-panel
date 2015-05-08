3142b61b1ceffc59f99cef56e2f120a8b95a2185
xuanhuan <1372460923@qq.com>
2015-05-08 19:24:48 +0800

    增加mail-smtp，新增网页用户数据表控件，优化显示配置文件！

--- user/node_json.php
+++ user/node_json.php
@@ -8,6 +8,16 @@ $method = $node->Method();
 $pass = $oo->get_pass();
 $port = $oo->get_port();
 ?>
+
+<!DOCTYPE html> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 
+<meta charset="utf-8" /> 
+<title>配置文件</title> 
+</head> 
+<body style="text-align:center"> 
+<h1>请复制下面内容！</h1>
+<textarea name="node_json" rows="9" cols="22" readonly style="resize:none" />
 {
 "server":"<?php echo $server; ?>",
 "server_port":<?php echo $port; ?>,
@@ -16,7 +26,16 @@ $port = $oo->get_port();
 "timeout":600,
 "method":"<?php echo $method; ?>"
 }
+</textarea>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-61550927-1', 'auto');
+  ga('send', 'pageview');
+</script>
 
-
-
+</body> 
+</html> 

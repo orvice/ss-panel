237708a058e14c425aba1a1d1a223d8af35e6f89
xuanhuan <1372460923@qq.com>
2015-05-10 15:53:26 +0800

    Revert "改进jquery.slimscroll.min.js区大小写导致加载失败，把jQuery.min.js移动到_main.php文件内！"
    This reverts commit b03deb1de36e5a0d46126b5697cbd2532c3561f3.

--- admin/_footer.php
+++ admin/_footer.php
@@ -14,10 +14,12 @@
 include_once '../ana.php';
 ?>
 
+<!-- jQuery 2.1.3 -->
+<script src="../asset/js/jQuery.min.js"></script>
 <!-- Bootstrap 3.3.2 JS -->
 <script src="../asset/js/bootstrap.min.js" type="text/javascript"></script>
 <!-- SlimScroll -->
-<script src="../asset/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+<script src="../asset/plugins/slimScroll/jquery.slimScroll.min.js" type="text/javascript"></script>
 <!-- FastClick -->
 <script src='../asset/plugins/fastclick/fastclick.min.js'></script>
 <!-- AdminLTE App -->

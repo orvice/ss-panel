58b2376bbd333f920942dd2e3eb5f208596b58b6
xuanhuan <1372460923@qq.com>
2015-05-16 18:26:17 +0800

    删除<!-- jQuery 2.1.3 --> <script src="../asset/js/jQuery.min.js"></script> 移到user/_main.php

--- user/_footer.php
+++ user/_footer.php
@@ -14,8 +14,6 @@
 include_once '../ana.php';
 ?>
 
-<!-- jQuery 2.1.3 -->
-<script src="../asset/js/jQuery.min.js"></script>
 <!-- Bootstrap 3.3.2 JS -->
 <script src="../asset/js/bootstrap.min.js" type="text/javascript"></script>
 <!-- SlimScroll -->

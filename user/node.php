a73be9a797637d3f3e31490c304b13a9c7541994
xuanhuan <1372460923@qq.com>
2015-05-16 10:30:38 +0800

    更新 node.php

--- user/node.php
+++ user/node.php
@@ -40,8 +40,8 @@ $node = new Ss\Node\Node();
                                                 操作 <span class="caret"></span>
                                             </a>
                                             <ul class="dropdown-menu">
-                                                <li role="presentation"><a role="menuitem" tabindex="-1" data-toggle="modal" data-target="#myModal" href="javascript:void(0)" data-url="node_json.php?id=<?php echo $row['id']; ?>">配置文件</a></li>
-                                                <li role="presentation"><a role="menuitem" tabindex="-1" data-toggle="modal" data-target="#myModal" href="javascript:void(0)" data-url="node_qr.php?id=<?php echo $row['id']; ?>">二维码</a></li>
+                                                <li role="presentation"><a role="menuitem" tabindex="-1" data-toggle="modal" data-target="#myModal" href="javascript:void(0)" data-url="node_json.php?id=<?php echo $row['id']; ?>" title="<?php echo $row['node_name']; ?>" >配置文件</a></li>
+                                                <li role="presentation"><a role="menuitem" tabindex="-1" data-toggle="modal" data-target="#myModal" href="javascript:void(0)" data-url="node_qr.php?id=<?php echo $row['id']; ?>" title="<?php echo $row['node_name']; ?>" >二维码</a></li>
                                             </ul>
                                         </li>
                                         <li class="pull-left header"><i class="fa fa-angle-right"></i> <?php echo $row['node_name']; ?></li>
@@ -84,8 +84,8 @@ $node = new Ss\Node\Node();
                                                 操作 <span class="caret"></span>
                                             </a>
                                             <ul class="dropdown-menu">
-                                                <li role="presentation"><a role="menuitem" tabindex="-1" data-toggle="modal" data-target="#myModal" href="javascript:void(0)" data-url="node_json.php?id=<?php echo $row['id']; ?>">配置文件</a></li>
-                                                <li role="presentation"><a role="menuitem" tabindex="-1" data-toggle="modal" data-target="#myModal" href="javascript:void(0)" data-url="node_qr.php?id=<?php echo $row['id']; ?>">二维码</a></li>
+                                                <li role="presentation"><a role="menuitem" tabindex="-1" data-toggle="modal" data-target="#myModal" href="javascript:void(0)" data-url="node_json.php?id=<?php echo $row['id']; ?>" title="<?php echo $row['node_name']; ?>" >配置文件</a></li>
+                                                <li role="presentation"><a role="menuitem" tabindex="-1" data-toggle="modal" data-target="#myModal" href="javascript:void(0)" data-url="node_qr.php?id=<?php echo $row['id']; ?>" title="<?php echo $row['node_name']; ?>" >二维码</a></li>
                                             </ul>
                                         </li>
                                         <li class="pull-left header"><i class="fa fa-angle-right"></i> <?php echo $row['node_name']; ?></li>

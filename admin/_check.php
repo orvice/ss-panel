25c4df0ab00c22828fe1a75aa91a0ebb7212313f
Acris <Acris@users.noreply.github.com>
2015-06-28 12:05:06 +0800

    修复通过注入cookie能够登录用户中心的BUG，建议所有用户立即更新。
    修复通过注入cookie能够登录用户中心的BUG，建议所有用户立即更新。

--- admin/_check.php
+++ admin/_check.php
@@ -1,23 +1,27 @@
 <?php
 //检测是否登录，若没登录则转向登录界面
-if(isset($_COOKIE['uid'])|| $_COOKIE['uid'] != ''){
-        //co
+if((isset($_COOKIE['uid'])|| $_COOKIE['uid'] != '') && (isset($_COOKIE['user_email'])|| $_COOKIE['user_email'] != '') && (isset($_COOKIE['user_pwd'])|| $_COOKIE['user_pwd'] != '')){
         $uid = $_COOKIE['uid'];
         $user_email = $_COOKIE['user_email'];
         $user_pwd  = $_COOKIE['user_pwd'];
-
         $U = new \Ss\User\UserInfo($uid);
         //验证cookie
         $pwd = $U->GetPasswd();
+        $email = $U->GetEmail();
         $pw = \Ss\User\Comm::CoPW($pwd);
-        if($pw != $user_pwd){
+        if($pw == $user_pwd && $email == $user_email){
+            $oo = new Ss\User\Ss($uid);
+            if(!$U->isAdmin()){
+                header("Location:../user/index.php");
+            }
+        } else {
+            setcookie("uid", "", time()-3600);
+            setcookie("user_email", "", time()-3600);
+            setcookie("user_pwd", "", time()-3600);
             header("Location:login.php");
-        }
-        if(!$U->isAdmin()){
-            header("Location:../user/index.php");
+            exit();
         }
 }else{
     header("Location:login.php");
     exit();
 }
-$oo = new Ss\User\Ss($uid);

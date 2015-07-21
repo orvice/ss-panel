7f4fbfabe5d19dce09e5b941a560350276fe979c
a1ickgu0 <a1ickgu0@icloud.com>
2015-07-22 00:14:53 +0800

    update tool list

--- index.php
+++ index.php
@@ -8,25 +8,31 @@ include_once 'header.php';
 
     <div class="jumbotron">
         <h2><?php echo $site_name; ?></h2>
-        <p class="lead"> 每个月5G流量，美国节点。</p>
+        <p class="lead"> 第个月5G流量，美国节点。</p>
         <p><a class="btn btn-lg btn-success" href="user/register.php" role="button">立即注册</a></p>
     </div>
 
     <div class="row marketing">
         <div class="col-lg-6">
-            <a href="https://play.google.com/store/apps/details?id=com.github.shadowsocks"><h4>Android</h4></a>
-            <p>Android客户端</p>
-            <h4><a href="http://sourceforge.net/projects/shadowsocksgui/files/dist/">Shadowsocks C#</a></h4>
-            <p> Windows用户推荐此客户端。</p>
+            <a href="https://itunes.apple.com/us/app/shadowsocks/id665729974?mt=8"><h4>iOS App Store </h4></a>
+            <p>iOS客户端</p>
+
+            <a href="https://github.com/linusyang/MobileShadowSocks/releases"><h4>iOS Jailbreak </h4></a>
+            <p>Cydia 上查找安装 "ShadowSocks" </p>
+
+            <h4><a href="https://github.com/shadowsocks/shadowsocks-iOS/releases">ShadowsocksX</a></h4>
+            <p> OS X用户推荐此客户端。</p>
         </div>
 
         <div class="col-lg-6">
-            <a href="https://itunes.apple.com/us/app/shadowsocks/id665729974?mt=8"><h4>iOS</h4></a>
-            <p>iOS客户端</p>
-
-            <h4><a href="https://github.com/ohdarling/GoAgentX/releases">GoAgentX</a></h4>
-            <p> OS X用户推荐此客户端。</p>
+            <a href="https://github.com/shadowsocks/shadowsocks-android/releases"><h4>Android</h4></a>
+            <p>Android客户端</p>
+            <h4><a href="https://github.com/shadowsocks/shadowsocks-csharp/releases">Shadowsocks csharp</a></h4>
+            <p> Windows用户推荐此客户端。</p>
+            <h4><a href="https://github.com/haohaolee/shadowsocks-openwrt">Openwrt</a></h4>
+            <p> 路由器安装(OpenWrt)</p>
         </div>
+
     </div><?php
             include_once 'footer.php';
             include_once 'ana.php';?>

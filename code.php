c5809aaad51ef5ec7f80291fdf1eef3b6583b287
orvice <orvice@gmail.com>
2015-07-25 21:44:23 +0800

    update code

--- code.php
+++ code.php
@@ -2,46 +2,58 @@
 require_once 'lib/config.php';
 include_once 'header.php';
 $c = new \Ss\User\Invite();
-?>
-<body>
-<div class="container">
-    <?php include_once 'nav.php';?>
+?>  
 
-    <div class="jumbotron">
-        <p class="lead"> 邀请码实时刷新</p>
-        <p>如遇到无邀请码请找已经注册的用户获取。</p>
-    </div>
-
-    <div class="row marketing">
-        <h2 class="sub-header">邀请码</h2>
-        <div class="table-responsive">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th>###</th>
-                    <th>邀请码</th>
-                    <th>状态</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $datas = $c->CodeArray();
-                $a = 0;
-                foreach($datas as $data ){
-                ?>
-                <tr>
-                    <td><?php echo $a;$a++; ?></td>
-                     <td><?php echo $data['code'];?></td>
-                    <td>可用</td>
-                </tr>
-                <?php } ?>
-                </tbody>
-            </table>
+<div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+        <br><br>
+        <div class="row center">
+            <h5  > 邀请码实时刷新</h5>
+            <h5>如遇到无邀请码请找已经注册的用户获取。</h5>
         </div>
-    </div><?php
-            include_once 'footer.php';
-            include_once 'ana.php';?>
+    </div>
+</div>
 
-</div> <!-- /container -->
-</body>
-</html>
+
+<div class="container">
+    <div class="section">
+
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="row marketing">
+                <h2 class="sub-header">邀请码</h2>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>###</th>
+                            <th>邀请码</th>
+                            <th>状态</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $datas = $c->CodeArray();
+                        $a = 0;
+                        foreach($datas as $data ){
+                            ?>
+                            <tr>
+                                <td><?php echo $a;$a++; ?></td>
+                                <td><?php echo $data['code'];?></td>
+                                <td>可用</td>
+                            </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br>
+
+    <div class="section">
+
+    </div>
+</div>
+<?php  include_once 'ana.php';
+include_once 'footer.php';?>

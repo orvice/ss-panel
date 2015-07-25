93079a866a1145e23c0b9b67b113894b7f65e78e
orvice <orvice@gmail.com>
2015-07-25 21:50:31 +0800

    update index

--- index.php
+++ index.php
@@ -27,18 +27,22 @@ include_once 'header.php';
             <div class="col s12 m4">
                 <div class="icon-block">
                     <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-                    <h5 class="center">Speeds up development</h5>
+                    <h5 class="center">Super Fast</h5>
 
-                    <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                    <p class="light">
+                        Bleeding edge techniques using Asynchronous I/O and Event-driven programming.
+                    </p>
                 </div>
             </div>
 
             <div class="col s12 m4">
                 <div class="icon-block">
                     <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-                    <h5 class="center">User Experience Focused</h5>
+                    <h5 class="center">Open Source</h5>
 
-                    <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                    <p class="light">
+                        Totally free and open source. A worldwide community devoted to deliver bug-free code and long-term support.
+                    </p>
                 </div>
             </div>
 
@@ -47,7 +51,9 @@ include_once 'header.php';
                     <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
                     <h5 class="center">Easy to work with</h5>
 
-                    <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                    <p class="light">
+                        Avaliable on multiple platforms, including PC, MAC, Mobile (Android and iOS) and Routers (OpenWRT).
+                    </p>
                 </div>
             </div>
         </div>

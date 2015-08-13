e88c51b633023cbb96442edc5d00f3c98837dcd2
xuanhuan <1372460923@qq.com>
2015-08-14 01:48:12 +0800

    Update user.php
    注册自定义插件 获取邀请人

--- admin/user.php
+++ admin/user.php
@@ -3,5 +3,17 @@ require_once '_main.php';
 $Users = new Ss\User\User();
 $us = $Users->AllUser();
 $smarty->assign('us',$us);
+
+// 注册自定义插件 获取邀请人
+$smarty->registerPlugin('function','get_ref_name','get_ref_name');
+function get_ref_name($rs,$content){
+    if ($rs['rs']['ref_by'] != 0){
+        $ref_u  = new \Ss\User\UserInfo($rs['rs']['ref_by']);
+        $ref_name = $ref_u->GetUserName();
+    	return $ref_name;
+    }else{
+    	return '<code class="hoverable">公开</code>';
+    }
+}
 $smarty->display('admin/user.tpl');
 ?>

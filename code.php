87b7e20cb5f0d3d40ed1239744570205bb4afc25
xuanhuan <1372460923@qq.com>
2015-08-20 01:20:25 +0800

    删除重复读取统计代码，全部由smarty.inc.php读取统计代码。

--- code.php
+++ code.php
@@ -2,7 +2,6 @@
 require_once 'lib/config.php';
 $c = new \Ss\User\Invite();
 $smarty->assign('c',$c);
-$smarty->assign('ana',file_get_contents("ana.php")); //读取统计代码
 $datas = $c->CodeArray();
 $smarty->assign('datas',$datas);
 $smarty->assign('a',$a=1);

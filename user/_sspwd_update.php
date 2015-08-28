3e54651324ca86228868272647db2d378ce68244
xuanhuan <1372460923@qq.com>
2015-08-28 08:56:15 +0800

    如果SS服务被停止，不能再修改Shadowsocks连接密码。

--- user/_sspwd_update.php
+++ user/_sspwd_update.php
@@ -2,15 +2,19 @@
 require_once '../lib/config.php';
 require_once '_check.php';
 
-// $pwd = $_POST['sspwd'];
-if($_POST['sspwd'] == ''){
-    $pwd = \Ss\Etc\Comm::get_random_char(8);
+if($oo->get_enable()){
+    if($_POST['sspwd'] == ''){
+        $pwd = \Ss\Etc\Comm::get_random_char(8);
+    }else{
+        $pwd = $_POST['sspwd'];
+        $pwd = htmlspecialchars($pwd, ENT_QUOTES, 'UTF-8');
+        $pwd = \Ss\Etc\Comm::checkHtml($pwd);
+    }
+    $oo->update_ss_pass($pwd);
+    $a['ok'] = '1';
+    $a['msg'] = "新密码为".$pwd;
 }else{
-    $pwd = $_POST['sspwd'];
-    $pwd = htmlspecialchars($pwd, ENT_QUOTES, 'UTF-8');
-    $pwd = \Ss\Etc\Comm::checkHtml($pwd);
+    $a['code'] = '0';
+    $a['msg']  = "你的SS服务已被停止，无法修改Shadowsocks连接密码。";
 }
-$oo->update_ss_pass($pwd);
-$a['ok'] = '1';
-$a['msg'] = "新密码为".$pwd;
-echo json_encode($a);
+echo json_encode($a,JSON_UNESCAPED_UNICODE);

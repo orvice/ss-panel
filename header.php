2a984f0ec4f4727aec5bde90950c07a412e8abcc
carvenli <carvenli@users.noreply.github.com>
2015-08-29 11:02:57 +0800

    fonts.googleapis.com替换为中科大的镜像

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
     <title><?php echo $site_name; ?></title> 
-    <!-- CSS  -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- CSS fonts.googleapis.com -->
+    <link href="//fonts.lug.ustc.edu.cn/icon?family=Material+Icons" rel="stylesheet">
     <link href="asset/materialize/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="asset/materialize/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>

d056cfa19e7f46b70337135b7d8cbebb9b733f91
xuanhuan <1372460923@qq.com>
2015-09-12 04:21:00 +0800

    加入防签到系统平台，如果不是在用户中心点的签到都不会奖励流量。

--- user/_checkin.php
+++ user/_checkin.php
@@ -1,8 +1,13 @@
 <?php
 require_once '../lib/config.php';
 require_once '_check.php';
+//检查是否在用户中心点的签到，如果不是就返回非法访问
+if($_SERVER['HTTP_REFERER']!=$site_url."user/index.php"){
+    $a['code'] = '0';
+    $a['msg'] = "非法访问";
+}
 //权限检查
-if(!$oo->is_able_to_check_in()){
+elseif(!$oo->is_able_to_check_in()){
     $transfer_to_add = 0;
 }else {
     if ($oo->unused_transfer() < 2048 * $tomb) {
@@ -12,7 +17,7 @@ if(!$oo->is_able_to_check_in()){
     }
     $oo->add_transfer($transfer_to_add*$tomb);
     $oo->update_last_check_in_time();
+    $a['msg'] = "获得了".$transfer_to_add."MB流量";
 }
 
-$a['msg'] = "获得了".$transfer_to_add."MB流量";
-echo json_encode($a);
+echo json_encode($a,JSON_UNESCAPED_UNICODE);

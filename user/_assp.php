f209e4a5b6a0f0b9689b98e8184697b894fe7311
xuanhuan <1372460923@qq.com>
2015-09-15 06:50:48 +0800

    Create _assp.php

---
+++ user/_assp.php
@@ -0,0 +1,8 @@
+<?php
+/* 防签到系统平台 处理 */
+//开启session
+session_start();
+//获取POST的ua,然后判断是否相等。
+if ($_SERVER['HTTP_USER_AGENT'].$_SESSION['js_ua_rand']==$_POST['ua']) {
+    $_SESSION['assp']=true;
+}

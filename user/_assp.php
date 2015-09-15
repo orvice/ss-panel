264ffd13d7cadaa6d965ab3e90e397993509ce0a
xuanhuan <1372460923@qq.com>
2015-09-16 03:50:30 +0800

    Update _assp.php

--- user/_assp.php
+++ user/_assp.php
@@ -5,5 +5,8 @@ session_start();
 //获取POST的ua,然后判断是否相等。
 if ($_SERVER['HTTP_USER_AGENT'].$_SESSION['js_ua_rand']==$_POST['ua']) {
     $_SESSION['assp']=true;
+//获取POST的uaid,然后判断是否相等。
+}elseif (base64_encode($_SESSION['js_ua_id'])==$_POST['uaid']) {
+    $_SESSION['assp']=true;
 }
 ?>

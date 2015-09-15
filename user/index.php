2ee378bead465c6277213b032aabc6dcff3fcf3b
xuanhuan <1372460923@qq.com>
2015-09-16 04:03:20 +0800

    Update index.php

--- user/index.php
+++ user/index.php
@@ -76,6 +76,7 @@ $unix_time = $oo->get_last_unix_time();
                         <div class="box-header">
                             <h3 class="box-title">签到获取流量</h3>
                         </div><!-- /.box-header -->
+                        <div id="assp"></div>
                         <div class="box-body">
                             <p> 22小时内可以签到一次。</p>
                             <?php  if($oo->is_able_to_check_in())  { ?>

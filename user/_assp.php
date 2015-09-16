12d6963951c194475bea2ff7e34e522936f761f9
xuanhuan <1372460923@qq.com>
2015-09-16 21:05:03 +0800

    Update _assp.php

--- user/_assp.php
+++ user/_assp.php
@@ -2,11 +2,8 @@
 /* 防签到系统平台 处理 */
 //开启session
 session_start();
-//获取POST的ua,然后判断是否相等。
-if ($_SERVER['HTTP_USER_AGENT'].$_SESSION['js_ua_rand']==$_POST['ua']) {
-    $_SESSION['assp']=true;
 //获取POST的uaid,然后判断是否相等。
-}elseif (base64_encode($_SESSION['js_ua_id'])==$_POST['uaid']) {
+if (base64_encode($_SESSION['js_ua_id'])==$_POST['uaid']) {
     $_SESSION['assp']=true;
 }
 ?>

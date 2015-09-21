b2ed0913274ca23b6f22b6da5a8da425e22a7150
xuanhuan <1372460923@qq.com>
2015-09-22 03:34:40 +0800

    把公告存储内容移到Sqlite，支持使用变量输出内容，不支持数组变量和其它操作。

--- code.php
+++ code.php
@@ -5,5 +5,6 @@ $smarty->assign('c',$c);
 $datas = $c->CodeArray();
 $smarty->assign('datas',$datas);
 $smarty->assign('a',$a=1);
+$smarty->assign("code_Announcement",Ss\ac::get('code_Announcement',get_defined_vars()));// 邀请码公告内容
 $smarty->display('code.tpl');
 ?>

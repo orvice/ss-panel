879923ce264eefb7fb1056e40561333540c0e47f
orvice <orvice@gmail.com>
2015-10-11 21:45:17 +0800

    add Cookie utils

---
+++ app/Utils/Cookie.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Utils;
+
+
+class Cookie
+{
+    public static function set($key,$value,$time){
+        setcookie($key,$value,$time);
+    }
+
+    public static function get($key){
+        return $_COOKIE[$key];
+    }
+}

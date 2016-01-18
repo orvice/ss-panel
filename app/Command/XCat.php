11980c6123e6674c4564021a01c61e7490b1f5d5
orvice <orvice@gmail.com>
2016-01-18 22:24:26 +0800

    init xcat

---
+++ app/Command/XCat.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Command;
+
+/***
+ * Class XCat
+ * @package App\Command
+ */
+
+class XCat
+{
+
+    public function defaultAction(){
+        echo "Memo";
+    }
+
+    public function install(){
+        echo "x cat will install ss-panel v3...../n";
+    }
+
+    public function addAdmin(){
+        echo "add admin";
+    }
+}

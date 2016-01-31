b9d35915bcf74d1f9c028d231327ee81dec24182
orvice <orvice@gmail.com>
2016-01-31 23:04:24 +0800

    move view to service

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controllers;
 
-use Smarty;
+
 
 use App\Services\Auth;
-use App\Services\Config;
+use App\Services\View;
+
 /**
  * BaseController
  */
@@ -22,16 +23,8 @@ class BaseController
     }
 
     public function smarty(){
-        //global $config;
-        $smarty=new smarty(); //实例化smarty
-        $smarty->settemplatedir(BASE_PATH.'/views/'.Config::get('theme').'/'); //设置模板文件存放目录
-        $smarty->setcompiledir(BASE_PATH.'/storage/framework/smarty/compile/'); //设置生成文件存放目录
-        $smarty->setcachedir(BASE_PATH.'/storage/framework/smarty/cache/'); //设置缓存文件存放目录
-        // add config
-        $smarty->assign('config',Config::getPublicConfig());
-        $smarty->assign('user',Auth::getUser());
-        $this->smarty = $smarty;
-        return $smarty;
+        $this->smarty = View::getSmarty();
+        return $this->smarty;
     }
 
     public function view(){

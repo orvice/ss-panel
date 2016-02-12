11e49e2e85eb8b29802e963821afb6e81abf3513
orvice <orvice@gmail.com>
2016-02-12 21:42:40 +0800

    bump version

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,7 @@ use App\Services\Boot;
 
 //  BASE_PATH
 define('BASE_PATH', __DIR__);
-define('VERSION', '3.1.2');
+define('VERSION', '3.1.3');
 
 // Vendor Autoload
 require BASE_PATH.'/vendor/autoload.php';

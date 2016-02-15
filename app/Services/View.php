e491a2e0e449254b02fd3b43b63c4c777e053ab5
orvice <orvice@gmail.com>
2016-02-15 21:48:27 +0800

    mv views to resources/views

--- app/Services/View.php
+++ app/Services/View.php
@@ -8,7 +8,7 @@ class View
 {
     public static function getSmarty(){
         $smarty=new smarty(); //实例化smarty
-        $smarty->settemplatedir(BASE_PATH.'/views/'.Config::get('theme').'/'); //设置模板文件存放目录
+        $smarty->settemplatedir(BASE_PATH.'/resources/views/'.Config::get('theme').'/'); //设置模板文件存放目录
         $smarty->setcompiledir(BASE_PATH.'/storage/framework/smarty/compile/'); //设置生成文件存放目录
         $smarty->setcachedir(BASE_PATH.'/storage/framework/smarty/cache/'); //设置缓存文件存放目录
         // add config

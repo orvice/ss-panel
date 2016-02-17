0eba781171c925744d26d1cad41b3bb7e67cd5de
orvice <orvice@gmail.com>
2016-02-17 20:53:38 +0800

    mu: user traffic update

--- app/Controllers/Mu/UserController.php
+++ app/Controllers/Mu/UserController.php
@@ -8,18 +8,39 @@ use App\Controllers\BaseController;
 class UserController extends BaseController
 {
     // User List
-    public function index($request, $response, $args){
+    public function index($request, $response, $args)
+    {
         $user = User::all();
         $res = [
             "ret" => 1,
             "msg" => "ok",
             "data" => $user
         ];
-        return $this->echoJson($response,$res);
+        return $this->echoJson($response, $res);
     }
 
     //   Update Traffic
-    public function addTraffic($request, $response, $args){
+    public function addTraffic($request, $response, $args)
+    {
+        $id = $args['id'];
+        $u = $request->getParam('u');
+        $d = $request->getParam('d');
+        $user = User::find($id);
 
+        $user->t = time();
+        $user->u = $user->u + $u;
+        $user->d = $user->d + $d;
+        if (!$user->save()) {
+            $res = [
+                "ret" => 0,
+                "msg" => "update failed",
+            ];
+            return $this->echoJson($response, $res);
+        }
+        $res = [
+            "ret" => 1,
+            "msg" => "ok",
+        ];
+        return $this->echoJson($response, $res);
     }
 }

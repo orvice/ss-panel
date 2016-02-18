56f37986bdee6a9fadb87b848af4e15b84c05af1
maxidea-com <maxidea@gmail.com>
2016-02-18 16:41:58 +0800

    bug fix

--- docker/mysql-init.sh
+++ docker/mysql-init.sh
@@ -8,7 +8,7 @@ else
 fi
 
 echo "create ss-panel database, user, password"
-mysql -u root -p 'pw123456' -e "CREATE DATABASE sspanel character SET utf8; CREATE user 'ssuser'@'localhost' IDENTIFIED BY 'sspasswd'; GRANT ALL privileges ON sspanel.* TO 'ssuser'@'localhost'; FLUSH PRIVILEGES;"
+mysql -uroot -p'pw123456' -e "CREATE DATABASE sspanel character SET utf8; CREATE user 'ssuser'@'localhost' IDENTIFIED BY 'sspasswd'; GRANT ALL privileges ON sspanel.* TO 'ssuser'@'localhost'; FLUSH PRIVILEGES;"
 
 echo "input shadowsocks sql init database"
 cd /opt/shadowsocks/shadowsocks; mysql -u ssuser -psspasswd sspanel < ./shadowsocks.sql

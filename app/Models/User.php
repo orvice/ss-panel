b8ef0effbfd2854ac34f47b81875e29ef1baf254
orvice <orvice@gmail.com>
2016-03-05 21:27:22 +0800

    fix ss-go
    fix https://github.com/orvice/shadowsocks-go/issues/14

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,50 +20,68 @@ class User extends Model
 
     public $isAdmin;
 
+    protected $casts = [
+        "t" => 'int',
+        "u" => 'int',
+        "d" => 'int',
+        "port" => 'int',
+        "transfer_enable" => 'int',
+        "enable" => 'int',
+        'is_admin' => 'boolean',
+    ];
+
     public function getGravatarAttribute()
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "https://secure.gravatar.com/avatar/$hash";
     }
 
-    public function isAdmin(){
+    public function isAdmin()
+    {
         return $this->attributes['is_admin'];
     }
 
-    public function lastSsTime(){
-        if($this->attributes['t'] == 0){
+    public function lastSsTime()
+    {
+        if ($this->attributes['t'] == 0) {
             return "从未使用喵";
         }
         return Tools::toDateTime($this->attributes['t']);
     }
 
-    public function lastCheckInTime(){
-        if($this->attributes['last_check_in_time'] == 0){
+    public function lastCheckInTime()
+    {
+        if ($this->attributes['last_check_in_time'] == 0) {
             return "从未签到";
         }
         return Tools::toDateTime($this->attributes['last_check_in_time']);
     }
 
-    public function regDate(){
+    public function regDate()
+    {
         return $this->attributes['reg_date'];
     }
 
-    public function updatePassword($pwd){
+    public function updatePassword($pwd)
+    {
         $this->pass = Hash::passwordHash($pwd);
         $this->save();
     }
 
-    public function updateSsPwd($pwd){
+    public function updateSsPwd($pwd)
+    {
         $this->passwd = $pwd;
         $this->save();
     }
 
-    public function updateMethod($method){
+    public function updateMethod($method)
+    {
         $this->method = $method;
         $this->save();
     }
 
-    public function addInviteCode(){
+    public function addInviteCode()
+    {
         $uid = $this->attributes['id'];
         $code = new InviteCode();
         $code->code = Tools::genRandomChar(32);
@@ -71,54 +89,63 @@ class User extends Model
         $code->save();
     }
 
-    public function addManyInviteCodes($num){
-        for($i = 0; $i < $num; $i++){
+    public function addManyInviteCodes($num)
+    {
+        for ($i = 0; $i < $num; $i++) {
             $this->addInviteCode();
         }
     }
 
-    public function trafficUsagePercent(){
+    public function trafficUsagePercent()
+    {
         $total = $this->attributes['u'] + $this->attributes['d'];
         $transfer_enable = $this->attributes['transfer_enable'];
-        $percent = $total/$transfer_enable;
-        $percent = round($percent,2);
-        $percent = $percent*100;
+        $percent = $total / $transfer_enable;
+        $percent = round($percent, 2);
+        $percent = $percent * 100;
         return $percent;
     }
 
-    public function enableTraffic(){
+    public function enableTraffic()
+    {
         $transfer_enable = $this->attributes['transfer_enable'];
         return Tools::flowAutoShow($transfer_enable);
     }
 
-    public function usedTraffic(){
+    public function usedTraffic()
+    {
         $total = $this->attributes['u'] + $this->attributes['d'];
         return Tools::flowAutoShow($total);
     }
 
-    public function unusedTraffic(){
+    public function unusedTraffic()
+    {
         $total = $this->attributes['u'] + $this->attributes['d'];
         $transfer_enable = $this->attributes['transfer_enable'];
-        return Tools::flowAutoShow($transfer_enable-$total);
+        return Tools::flowAutoShow($transfer_enable - $total);
     }
 
-    public function isAbleToCheckin(){
+    public function isAbleToCheckin()
+    {
         $last = $this->attributes['last_check_in_time'];
         $hour = Config::get('checkinTime');
-        if($last + $hour*3600 < time() ){
+        if ($last + $hour * 3600 < time()) {
             return true;
         }
         return false;
     }
+
     /*
      * @param traffic 单位 MB
      */
-    public function addTraffic($traffic){
+    public function addTraffic($traffic)
+    {
     }
 
-    public function inviteCodes(){
+    public function inviteCodes()
+    {
         $uid = $this->attributes['id'];
-        return InviteCode::where('user_id',$uid)->get();
+        return InviteCode::where('user_id', $uid)->get();
     }
 
 }

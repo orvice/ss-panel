dfb0e3076f104a0449e25ded274acaefb8badc6f
orvice <orvice@gmail.com>
2016-03-06 13:49:11 +0800

    add all users traffic log in admin

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -2,27 +2,26 @@
 
 namespace App\Controllers;
 
-use App\Models\InviteCode;
-use App\Models\Node;
+use App\Models\InviteCode, App\Models\Node, App\Models\TrafficLog;
 use App\Utils\Tools;
 use App\Services\Analytics;
 
 /**
  *  Admin Controller
  */
-
 class AdminController extends BaseController
 {
 
     public function index()
     {
         $sts = new Analytics();
-        return $this->view()->assign('sts',$sts)->display('admin/index.tpl');
+        return $this->view()->assign('sts', $sts)->display('admin/index.tpl');
     }
 
-    public function node(){
+    public function node()
+    {
         $nodes = Node::all();
-        return $this->view()->assign('nodes',$nodes)->display('admin/node.tpl');
+        return $this->view()->assign('nodes', $nodes)->display('admin/node.tpl');
     }
 
     public function sys()
@@ -32,29 +31,40 @@ class AdminController extends BaseController
 
     public function invite()
     {
-        $codes = InviteCode::where('user_id','=','0')->get();
-        return $this->view()->assign('codes',$codes)->display('admin/invite.tpl');
+        $codes = InviteCode::where('user_id', '=', '0')->get();
+        return $this->view()->assign('codes', $codes)->display('admin/invite.tpl');
     }
 
     public function addInvite($request, $response, $args)
     {
-        $n =  $request->getParam('num');
+        $n = $request->getParam('num');
         $prefix = $request->getParam('prefix');
-        $uid =  $request->getParam('uid');
-        if ($n < 1){
+        $uid = $request->getParam('uid');
+        if ($n < 1) {
             $res['ret'] = 0;
             return $response->getBody()->write(json_encode($res));
         }
-        for ($i = 0; $i < $n; $i++ ){
+        for ($i = 0; $i < $n; $i++) {
             $char = Tools::genRandomChar(32);
             $code = new InviteCode();
-            $code->code = $prefix.$char;
+            $code->code = $prefix . $char;
             $code->user_id = $uid;
             $code->save();
         }
         $res['ret'] = 1;
         $res['msg'] = "邀请码添加成功";
         return $response->getBody()->write(json_encode($res));
+    }
+
+    public function trafficLog($request, $response, $args)
+    {
+        $pageNum = 1;
+        if (isset($request->getQueryParams()["page"])) {
+            $pageNum = $request->getQueryParams()["page"];
+        }
+        $traffic = TrafficLog::orderBy('id', 'desc')->paginate(15, ['*'], 'page', $pageNum);
+        $traffic->setPath('/admin/trafficlog');
+        return $this->view()->assign('logs', $traffic)->display('admin/trafficlog.tpl');
     }
 
 }

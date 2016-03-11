d41cf7a8eb63fb038166ea55c51f477862639359
orvice <orvice@gmail.com>
2016-03-11 21:21:26 +0800

    update db

--- db-160311.sql
+++ db-160311.sql
@@ -5,6 +5,17 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `sp_config`;
+CREATE TABLE `sp_config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(128) NOT NULL,
+  `value` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 DROP TABLE IF EXISTS `ss_invite_code`;
 CREATE TABLE `ss_invite_code` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -110,4 +121,4 @@ CREATE TABLE `user_traffic_log` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2016-03-05 15:11:18
+-- 2016-03-11 13:21:01

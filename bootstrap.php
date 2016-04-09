0528e88a3241aa1f39312dd789d0e74f8f8ba5cc
orvice <orvice@gmail.com>
2016-04-09 15:54:25 +0800

    update version

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,7 @@ use App\Services\Boot;
 
 //  BASE_PATH
 define('BASE_PATH', __DIR__);
-define('VERSION', '3.3.8');
+define('VERSION', '3.3.9');
 
 // Vendor Autoload
 require BASE_PATH . '/vendor/autoload.php';

fe818e962900634f66981cda1ee9fb3d57e0969e
orvice <orvice@gmail.com>
2016-04-15 00:12:04 +0800

    update api test

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -7,4 +7,10 @@ class ApiTest extends TestCase
         $this->get('/api');
         $this->assertEquals('404', $this->response->getStatusCode());
     }
+
+    public function testUnauthorized()
+    {
+        $this->get("/api/node");
+        $this->assertEquals('401', $this->response->getStatusCode());
+    }
 }

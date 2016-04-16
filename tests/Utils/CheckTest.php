aca7a6c311d7f09832561fedb915b2ae0ac687fe
orvice <orvice@gmail.com>
2016-04-16 22:55:59 +0800

    test check ip limit func

--- tests/Utils/CheckTest.php
+++ tests/Utils/CheckTest.php
@@ -9,4 +9,10 @@ class CheckTest extends TestCase
         $this->assertEquals(false, Check::isEmailLegal("illegalEmail"));
         $this->assertEquals(true, Check::isEmailLegal("sspanel@gmail.com"));
     }
+
+    public function testGetRegIpLimit()
+    {
+        $count = Check::getIpRegCount('255.255.255.255');
+        $this->assertEquals(0, $count);
+    }
 }

b5bdf9fc7b7cf8ccfe60d61523071b5ac49c3d25
orvice <orvice@gmail.com>
2016-05-02 12:44:26 +0800

    add user test

---
+++ tests/Admin/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Tests\Admin;
+
+
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testUserList()
+    {
+        $this->get('/admin/user');
+        $this->assertEquals('200', $this->response->getStatusCode());
+    }
+
+    public function testUserInfo()
+    {
+        $this->get('/admin/user/1/edit');
+        $this->assertEquals('200', $this->response->getStatusCode());
+    }
+}

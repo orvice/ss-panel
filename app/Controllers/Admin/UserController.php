b212b79dde567e2f4d2fa14db1e375d626f507c6
https://github.com/monokoo <realstones2012@gmail.com>
2016-07-11 19:04:44 +0800

    fix node table

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -16,6 +16,7 @@ class UserController extends AdminController
         if (isset($request->getQueryParams()["page"])) {
             $pageNum = $request->getQueryParams()["page"];
         }
+		$nodes = Node::all();
         $users = User::paginate(15, ['*'], 'page', $pageNum);
         $users->setPath('/admin/user');
         return $this->view()->assign('users', $users)->display('admin/user/index.tpl');

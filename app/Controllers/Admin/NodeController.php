41d7048b4bc467a75ad937b496aab6641a79f733
monokoo <realstones2012@gmail.com>
2016-07-13 13:28:26 +0800

    fix issue

--- app/Controllers/Admin/NodeController.php
+++ app/Controllers/Admin/NodeController.php
@@ -86,7 +86,7 @@ class NodeController extends AdminController
         $rs['msg'] = "修改成功";
         return $response->getBody()->write(json_encode($rs));
     }
-    public function update($request, $response, $args)
+    public function update1($request, $response, $args)
     {
         $id = $args['id'];
 		    $user = User::find($id);	

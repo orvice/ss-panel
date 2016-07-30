4846f33c2e94bcbe6f396ef7de304a17e637164a
Tony Liu <388848@163.com>
2016-07-30 15:39:02 +0800

    Introduce SendCloud email service support. Signed-off-by: Tony Liu <388848@163.com>

--- app/Services/Mail.php
+++ app/Services/Mail.php
@@ -10,6 +10,7 @@ use App\Services\Mail\File;
 use App\Services\Mail\Mailgun;
 use App\Services\Mail\Ses;
 use App\Services\Mail\Smtp;
+use App\Services\Mail\SendCloud;
 use Smarty;
 
 
@@ -28,6 +29,8 @@ class Mail
                 return new Ses();
             case "smtp":
                 return new Smtp();
+            case "sendcloud":
+                return new SendCloud();
             case "file":
                 return new File();
             default:
---
+++ app/Services/Mail/SendCloud.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services\Mail;
+
+use App\Services\Config;
+use SendCloud\SendCloud as SendCloudService;
+
+class SendCloud extends Base
+{
+    private $config, $sc;
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function __construct()
+    {
+        $this->config = $this->getConfig();
+        $this->sc = new SendCloudService();
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getConfig()
+    {
+        return [
+            "key" => Config::get('sendcloud_key'),
+            "user" => Config::get('sendcloud_user'),
+            "sender" => Config::get('sendcloud_sender')
+        ];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function send($to, $subject, $text, $file)
+    {
+		$req = $this->sc->prepare('mail', 'send', [
+			'apiUser' => $this->config["user"],
+			'apiKey' => $this->config["key"],
+			'from' => $this->config["sender"],
+			'to' => $to,
+			'subject' => $subject,
+			'html' => $text
+		]);
+		$req->send();
+    }
+};

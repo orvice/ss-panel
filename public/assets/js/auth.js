e8696e6b650680a802a2e82d1203deb6ccc5c67b
orvice <orvice@gmail.com>
2017-03-02 21:37:16 +0800

    update js

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -63,14 +63,20 @@
 /******/ 	__webpack_require__.p = "./";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-(function webpackMissingModule() { throw new Error("Cannot find module \"/Users/orvice/www/ss-panel/resources/assets/js/auth.js\""); }());
+module.exports = __webpack_require__(0);
 
 
 /***/ })

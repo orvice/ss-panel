94ee4daa067edbc7380f21b9788ee139427aae3a
LI JIAHAO <lijiahao99131@gmail.com>
2017-04-05 22:34:32 +0800

    add support for new cipher

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -78,11 +78,22 @@ class Node extends Model
             "aes-128-cfb" => "aes-128-cfb",
             "aes-192-cfb" => "aes-192-cfb",
             "aes-256-cfb" => "aes-256-cfb",
+            "aes-128-ctr" => "aes-128-ctr",
+            "aes-192-ctr" => "aes-192-ctr",
+            "aes-256-ctr" => "aes-256-ctr",
             "des-cfb" => "des-cfb",
             "bf-cfb" => "bf-cfb",
             "cast5-cfb" => "cast5-cfb",
+            "camellia-128-cfb" => "camellia-128-cfb",
+            "camellia-192-cfb" => "camellia-192-cfb",
+            "camellia-256-cfb" => "camellia-256-cfb",
             "chacha20" => "chacha20",
             "salsa20" => "salsa20",
+            "chacha20-ietf" => "chacha20-ietf",
+            "aes-128-gcm" => "aes-128-gcm",
+            "aes-192-gcm" => "aes-192-gcm",
+            "aes-256-gcm" => "aes-256-gcm",
+            "chacha20-ietf-poly1305" => "chacha20-ietf-poly1305",
         ];
     }
 }

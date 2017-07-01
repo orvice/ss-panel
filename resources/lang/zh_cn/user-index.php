0c593727df3a40f6833c1ebf8b31b52f61dcde9b
Ziyao Zhang <ziyaointl@gmail.com>
2017-07-01 23:08:52 +0800

    Translate user-index.php to Chinese

---
+++ resources/lang/zh_cn/user-index.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'announcement' => '公告',
+    'faq' => '常见问题',
+
+    'checkin' => '签到',
+    'checkin-des' => "每 %s 小时可以签到一次。",
+    'cant-checkin' => "无法签到",
+    'last-checkin-at' => '上次签到时间',
+    'traffic-got' => '你获得了 ',
+
+    'traffic-info' => '流量使用情况',
+    'traffic-total' => '总流量',
+    'traffic-used' => '已用流量',
+    'traffic-unused' => '剩余流量',
+
+    'connection-info' => '链接信息',
+
+    'last-used' => '上次使用于',
+];

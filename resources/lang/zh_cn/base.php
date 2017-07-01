2f3e6cc0f1457316de29be34ef291e5d41a079fd
Ziyao Zhang <ziyaointl@gmail.com>
2017-07-01 23:02:08 +0800

    Translate base.php to Chinese

---
+++ resources/lang/zh_cn/base.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "success" => "成功",
+    "error" => "错误",
+    "something-wrong" => "有东西出错了",
+    "system-error" => "系统错误",
+
+    'setting' => "设置",
+    'update' => "更新",
+
+    'never' => "从不",
+];

8a0e0c6450b8a2b6d3a065e2442f8259b4483d1a
orvice <orvice@gmail.com>
2017-07-06 23:09:22 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+echo "Entry ss-panel"
+if [ "$MIGRATION" = "false" ];then
+    echo "Skip database migration"
+else
+    echo "start database migration"
+    php xcat migrate
+fi
+
+
+if [ "$ADMIN_EMAIL" != "" ] || [ "$ADMIN_PASS" != "" ];then
+  echo "create admin"
+else
+  echo "skip create admin"
+  php xcat createAdmin "$ADMIN_EMAIL" "$ADMIN_PASS"
+fi

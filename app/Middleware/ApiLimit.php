788cceed0ec929fd919f427ea257fac7f57590c2
orvice <orvice@gmail.com>
2017-07-08 00:27:14 +0800

    Update .travis.yml

---
+++ app/Middleware/ApiLimit.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Middleware;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class ApiLimit
+{
+    use Helper;
+
+    public function __invoke(Request $request, Response $response, $next)
+    {
+
+    }
+}

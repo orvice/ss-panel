a50550c5a453e31f33a944c63cf8bd56b9ebf333
orvice <orvice@gmail.com>
2017-07-09 03:22:55 +0800

    #719

--- config/auth.php
+++ config/auth.php
@@ -7,7 +7,7 @@
 return [
     'password_encryption_type' => env('AUTH_PASSWORD_ENCRYPTION_TYPE', 'bcrypt'),
     'salt' => env("AUTH_SALT", ''),
-    'session_timeout' => 3600 * 24,
+    'session_timeout' => env("TOKEN_TTL", 3600 * 24),
     'email_verify_enabled' => env('APP_EmailVerifyEnabled', false),
     'is_enable_google_login' => env('AUTH_IS_ENABLE_GOOGLE_LOGIN', false),
     'is_enable_facebook_login' => env('AUTH_IS_ENABLE_FACEBOOK_LOGIN', false),
